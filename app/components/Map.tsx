4e119fcf51e048c6a9ce53178d37c1a589171364
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2025-04-05 23:25:11 +0200

    make marker draggable

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -9,33 +9,78 @@ import {
   Marker,
   Popup,
   TileLayer,
+  Tooltip,
+  useMap,
   useMapEvents,
 } from "react-leaflet";
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 function NewMarker() {
   const [position, setPosition] = useState<L.LatLng | null>(null);
   const [obstacleType, setObstacleType] = useState("");
+  const [draggable, setDraggable] = useState(false);
+  const markerRef = useRef(null);
+
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current;
+        if (marker != null) {
+          setPosition(marker.getLatLng());
+        }
+      },
+    }),
+    []
+  );
+
+  const toggleDraggable = useCallback(() => {
+    setDraggable((d) => !d);
+  }, []);
 
   const map = useMapEvents({
     click(event) {
-      const userInput = prompt("What's the obstacle type?");
-      if (userInput === null) {
+      const inputtedObstacleType = prompt("What's the obstacle type?");
+      if (inputtedObstacleType === null) {
         // User cancelled the prompt, do nothing
         return;
       }
-      setObstacleType(userInput);
+      setObstacleType(inputtedObstacleType);
       setPosition(event.latlng);
       map.flyTo(event.latlng, map.getZoom());
     },
   });
 
+  const lat = position?.lat.toFixed(4);
+  const lng = position?.lng.toFixed(4);
+
+  const tooltipText = `Obstacle type: ${obstacleType}, latitude: ${lat}, longitude: ${lng}`;
+
   return (
     position && (
-      <Marker position={position}>
-        <Popup>{obstacleType}</Popup>
+      <Marker
+        draggable={draggable}
+        position={position}
+        eventHandlers={eventHandlers}
+        ref={markerRef}
+      >
+        <Popup minWidth={90}>
+          <span onClick={toggleDraggable}>
+            {draggable
+              ? "Marker is draggable"
+              : "Click here to make marker draggable"}
+          </span>
+        </Popup>
+        <Tooltip permanent>{tooltipText}</Tooltip>
       </Marker>
     )
+  );
+}
+
+function MapPlaceholder() {
+  return (
+    <p>
+      <noscript>You need to enable JavaScript to see this map.</noscript>
+    </p>
   );
 }
 
@@ -45,13 +90,15 @@ export default function Map() {
       center={{ lat: 51.505, lng: -0.09 }}
       zoom={20}
       style={{ height: "100%", width: "100%" }}
+      placeholder={<MapPlaceholder />}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
       <NewMarker />
     </MapContainer>
   );
 }
+
+// TODO  • Save lat, lng, type to PostgreSQL table obstacles
